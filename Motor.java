baad370ac4538d6148b48fc082012904617f0eff
Sam Nystrom <samuel.l.nystrom@gmail.com>
2022-07-01 14:36:14 -0400

    width

--- Motor.java
+++ Motor.java
@@ -2,7 +2,7 @@ public class Motor {
     Motor() {}
     /** @param x Should be in 0 - 1 range */
     public void setPower(double x) {
-        final int n = 64;
+        final int n = 72;
         final String s = "#".repeat(n);
         final String h = "-".repeat(n);
         System.out.printf("[%s%s] %d%%\n", s.substring(0, (int)(x * s.length())), h.substring((int)(x * h.length()), h.length()), (int)(x * 100.0));
